11347460d5ff5b5d4f0ef552831e299f1982043e
ddermendzhiev <cdermendzhiev13@gmail.com>
2024-11-09 17:25:33 -0500

    gdb log analysis recovered

--- src/angr_script.py
+++ src/angr_script.py
@@ -1,18 +1,23 @@
 import angr
 import time
 import sys
+import os
 import ipdb
+import multiprocessing
+import time
 
 import logging
 # logging.getLogger('angr').setLevel(logging.DEBUG)
 
+sys.path.append('../')
 from log_parsing.parser_perf import parse_perf_script_output
 from log_parsing.parser_gdb import parse_gdb_log
 from pretty_print import print_msg_box
 
 
 grep_path = '/home/dinko/exec-proj/grep/grep-3.11/src/grep'
-grep_perf_path = '/home/dinko/exec-proj/grep/perf_script_dynamic.log'
+grep_perf_log = '..../perf_script_dynamic.log'
+grep_gdb_log = '/home/dinko/exec-proj/log/grep/function_trace_BRsrc.log'
 
 bin_path = grep_path
 
@@ -20,14 +25,23 @@ bin_path = grep_path
 
 """ Log Parsing """
 
-# 'perf script' log
-syscall_data = parse_perf_script_output(grep_perf_path)
+# LOG: 'perf script'
+"""syscall_data = parse_perf_script_output(grep_perf_log)
 
 for entry in syscall_data:
     print(f"Syscall: {entry['syscall']}, Callsite: {entry['callsite']}")
-print_msg_box(f"===== {len(syscall_data)} syscall callsites =====")
+print_msg_box(f"===== {len(syscall_data)} syscall callsites =====")"""
 
 
+# LOG: gdb func trace
+gdb_logs = parse_gdb_log(grep_gdb_log)
+base_address = 0x555555554005
+base_angr = 0x400000
+
+for entry in gdb_logs[1:]:
+    entry['Addr'] = hex(int(entry['Addr'], 16) - base_address + base_angr)
+    print(f"{entry['Addr']}, {entry['Func']}")
+print_msg_box(f"===== {len(gdb_logs)} callsites =====")
 
 
 
@@ -35,7 +49,7 @@ print_msg_box(f"===== {len(syscall_data)} syscall callsites =====")
 
 """ ANGR Analysis"""
 
-proj = angr.Project(bin_path, load_options={'auto_load_libs': True})
+proj = angr.Project(bin_path, load_options={'auto_load_libs': False})
 
 # Find the address of 'main'
 main_symbol = proj.loader.find_symbol('main')
@@ -43,54 +57,82 @@ main_addr = main_symbol.rebased_addr
 
 # Generate CFG
 start = time.time()
-cfg = proj.analyses.CFGFast(resolve_indirect_jumps=True)
+cfg = proj.analyses.CFGFast()
 end = time.time()
 print(f"CFGFast: {cfg.graph}, {round(end - start, 2)}s")
-# grep (dynamically linked): CFGFast: DiGraph with 131172 nodes and 361298 edges, 275.39s
+# grep: CFGFast: DiGraph with 131172 nodes and 361298 edges, 275.39s
 
 
 
-# # dynamic CFG (symbolic)
-# start = time.time()
-# cfg = proj.analyses.CFGEmulated(
-#     starts=[main_addr],  # Start from main
-#     context_sensitivity_level=2,  # Increase context sensitivity
-#     keep_state=True,  # Keep the state to resolve indirect jumps
-#     call_depth=20,
-#     normalize=True,
-#     enable_function_hints=True  # Disable hints to force analysis
-# )
-# end = time.time()
-# print(f"CFGEmulated: {cfg.graph}, {round(end - start, 2)}")
+# dynamic CFG (symbolic)
+"""start = time.time()
+cfg = proj.analyses.CFGEmulated(
+    starts=[main_addr],  # Start from main
+    context_sensitivity_level=2,  # Increase context sensitivity
+    keep_state=True,  # Keep the state to resolve indirect jumps
+    call_depth=20,
+    normalize=True,
+    enable_function_hints=True  # Disable hints to force analysis
+)
+end = time.time()
+print(f"CFGEmulated: {cfg.graph}, {round(end - start, 2)}")"""
 
-ltrace = [0x980a, 0x907b, 0x9317, 0x866b, 0x84a6]
-ltrace = [i + 0x400000 for i in ltrace]
-
-# Check if the callsites exist in the CFG
-for entry in syscall_data:
-    node = cfg.model.get_any_node(entry['callsite'], anyaddr=True)
-    if node:
-        print(f"Callsite at {hex(int(entry['callsite'], 16))} exists in CFG.")
-    else:
-        print(f"Callsite at {hex(int(entry['callsite'], 16))} not found in CFG.")
-
-for entry in ltrace:
-    node = cfg.model.get_any_node(entry, anyaddr=True)
-    if node:
-        print(f"Callsite at {hex(entry)} exists in CFG.")
-    else:
-        print(f"Callsite at {hex(entry)} not found in CFG.")
-
-ipdb.set_trace()
-
-
-
-# Reconstruct the Execution Path
 execution_path = []
 prev_addr = cfg.kb.functions.function(name='main').addr
 prev_call = "main"
 print_msg_box(f"MAIN address: {hex(prev_addr)}")
 
+# Check if the callsites exist in the CFG
+for entry in gdb_logs[1:]:
+    addr = int(entry['Addr'], 16)
+    addr_str = hex(addr)
+    func_name = entry['Func']
+    node = cfg.model.get_any_node(addr, anyaddr=True)
+    if node:
+        print(f"Callsite {addr_str} exists in CFG.")
+    else:
+        print(f"Callsite {addr_str} not found in CFG.")
+
+
+ipdb.set_trace()
+
+
+for entry in gdb_logs[1:]:
+    target_addr = int(entry['Addr'], 16)
+    target_addr_str = hex(addr)
+    func_name = entry['Func']
+    print(f"Finding path from {hex(prev_addr)} ({prev_call})  to {target_addr_str} ({func_name})")
+    
+    # Find a path from prev_addr to syscall_addr
+    try:
+        start_state = proj.factory.blank_state(addr=prev_addr)
+        simgr = proj.factory.simgr(start_state)
+        simgr.explore(find=target_addr)
+
+        ipdb.set_trace()
+        
+        if simgr.found:
+            print(f"\t--> {len(simgr.found)} paths found")
+            found_path = simgr.found[0]
+            # Extract the list of basic block addresses traversed
+            trace = found_path.history.bbl_addrs.hardcopy
+            trace = [hex(i) for i in trace]
+            print(f"\t--> Trace {trace}")
+            execution_path.extend(trace)
+        else:
+            print(f"\t--> No path found from {hex(prev_addr)} to {target_addr_str}")
+    except Exception as e:
+        print(f"Error finding path to {target_addr_str}: {e}")
+    finally:
+        prev_addr = target_addr  # Update for next syscall
+        prev_call = func_name 
+
+ipdb.set_trace()
+
+
+
+# ltrace logs
+"""
 for entry in ltrace:
     syscall_addr = entry
     syscall_name = 'None'
@@ -118,36 +160,38 @@ for entry in ltrace:
         prev_addr = syscall_addr  # Update for next syscall
         prev_call = syscall_name 
     except Exception as e:
-        print(f"Error finding path to {hex(syscall_addr)}: {e}")
+        print(f"Error finding path to {hex(syscall_addr)}: {e}")"""
 
-# for entry in syscall_data:
-#     syscall_addr = int(entry['callsite'], 16)
-#     syscall_name = entry['syscall']
-#     print(f"Finding path from {hex(prev_addr)} ({prev_call})  to {hex(syscall_addr)} ({syscall_name})")
+# PERF syscall logs
+"""for entry in syscall_data:
+    syscall_addr = int(entry['callsite'], 16)
+    syscall_name = entry['syscall']
+    print(f"Finding path from {hex(prev_addr)} ({prev_call})  to {hex(syscall_addr)} ({syscall_name})")
     
-#     # Find a path from prev_addr to syscall_addr
-#     try:
-#         start_state = proj.factory.blank_state(addr=prev_addr)
-#         simgr = proj.factory.simgr(start_state)
-#         simgr.explore(find=syscall_addr)
+    # Find a path from prev_addr to syscall_addr
+    try:
+        start_state = proj.factory.blank_state(addr=prev_addr)
+        simgr = proj.factory.simgr(start_state)
+        simgr.explore(find=syscall_addr)
 
-#         ipdb.set_trace()
+        ipdb.set_trace()
         
-#         if simgr.found:
-#             print(f"\t--> {len(simgr.found)} paths found")
-#             found_path = simgr.found[0]
-#             # Extract the list of basic block addresses traversed
-#             trace = found_path.history.bbl_addrs.hardcopy
-#             trace = [hex(i) for i in trace]
-#             print(f"\t--> Trace {trace}")
-#             execution_path.extend(trace)
-#             # prev_addr = syscall_addr  # Update for next syscall
-#         else:
-#             print(f"\t--> No path found from {hex(prev_addr)} to {hex(syscall_addr)}")
-#         prev_addr = syscall_addr  # Update for next syscall
-#         prev_call = syscall_name 
-#     except Exception as e:
-#         print(f"Error finding path to {hex(syscall_addr)}: {e}")
+        if simgr.found:
+            print(f"\t--> {len(simgr.found)} paths found")
+            found_path = simgr.found[0]
+            # Extract the list of basic block addresses traversed
+            trace = found_path.history.bbl_addrs.hardcopy
+            trace = [hex(i) for i in trace]
+            print(f"\t--> Trace {trace}")
+            execution_path.extend(trace)
+            # prev_addr = syscall_addr  # Update for next syscall
+        else:
+            print(f"\t--> No path found from {hex(prev_addr)} to {hex(syscall_addr)}")
+        prev_addr = syscall_addr  # Update for next syscall
+        prev_call = syscall_name 
+    except Exception as e:
+        print(f"Error finding path to {hex(syscall_addr)}: {e}")"""
+
 
 # # Remove duplicates while preserving order
 # seen = set()
@@ -156,6 +200,7 @@ for entry in ltrace:
 #     if addr not in seen:
 #         seen.add(addr)
 #         ordered_execution_path.append(addr)
+
 
 # Display the execution path
 print("Reconstructed Execution Path:")
