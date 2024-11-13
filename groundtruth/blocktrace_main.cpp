bbeed9a681bb87e6f64edf64a41e0cd21d2005e7
ddermendzhiev <cdermendzhiev13@gmail.com>
2024-11-13 12:36:19 -0500

    add function attribution to pintool

--- groundtruth/blocktrace_main.cpp
+++ groundtruth/blocktrace_main.cpp
@@ -19,11 +19,21 @@ ofstream DBGFile;
 // make it static to help the compiler optimize docount
 static UINT64 icount = 0;
 
+// Global map to store function names
+std::map<ADDRINT, std::string> func_name_map;
+
 const ADDRINT base_address = 0x555555554005;
+const ADDRINT angr_base = 0x400000;
 // This function is called before every block
-VOID trace_bbl(ADDRINT addr)
+VOID trace_bbl(ADDRINT addr, ADDRINT rtn_addr)
 {
-    OutFile << "0x" << std::hex << (addr - base_address) << endl;
+    OutFile << "0x" << std::hex << (addr - base_address);
+    if (rtn_addr != 0 && func_name_map.count(rtn_addr) > 0) {
+        OutFile << " --> " << func_name_map[rtn_addr];
+    } else {
+        OutFile << " --> (unknown)";
+    }
+    OutFile << endl;
 }
 
 // Pin calls this function every time a new basic block is encountered
@@ -41,39 +51,41 @@ VOID Trace(TRACE trace, VOID* v)
     if (is_valid) {
         is_main = IMG_IsMainExecutable(current_image);
         if (is_main) {
-            DBGFile << "main -> 0x" << std::hex << IMG_LowAddress(current_image) << endl;
+            DBGFile << "main -> 0x" << std::hex << IMG_LowAddress(current_image);
         } else {
-            DBGFile << "----- NOT main -----" << endl;
+            DBGFile << "----- NOT main -----";
         }
     } else {
-        DBGFile << "----- invalid -----" << endl;
+        DBGFile << "----- invalid -----";
     }
 
     /*
     * RTN routine name analysis
     */
-    // RTN rtn = TRACE_Rtn(trace);
-    // if (RTN_Valid(rtn)) {
-    //     const std::string name = RTN_Name(rtn);
-    //     DBGFile << "name: " << name << endl;
-    // } else {
-    //     DBGFile << "INVALID rtn" << endl;
-    // }
-    // else {
-    //     std::cout << "is not main" << endl;
-    // }
+    RTN rtn = TRACE_Rtn(trace);
+    std::string func_name = "(unknown)";
+    ADDRINT rtn_addr = 0;
+    if (RTN_Valid(rtn)) {
+        func_name = RTN_Name(rtn);
+        rtn_addr = RTN_Address(rtn);
+        // Store function name with address as key
+        func_name_map[rtn_addr] = func_name;
+        DBGFile << "\tfunc: " << func_name << ", addr: " << rtn_addr << endl;
+    } else {
+        DBGFile << "\tINVALID rtn_name" << endl;
+    }
 
     if (is_valid && is_main) {
         for (BBL bbl = TRACE_BblHead(trace); BBL_Valid(bbl); bbl = BBL_Next(bbl))
         {
             ADDRINT addr = BBL_Address(bbl);
             // Insert a call to docount before every bbl, passing the number of instructions
-            BBL_InsertCall(bbl, IPOINT_BEFORE, (AFUNPTR)trace_bbl, IARG_UINT64, addr, IARG_END);
+            BBL_InsertCall(bbl, IPOINT_BEFORE, (AFUNPTR)trace_bbl, IARG_UINT64, addr, IARG_UINT64, rtn_addr, IARG_END);
         }
     }
 }
 
-KNOB< string > KnobOutputFile(KNOB_MODE_WRITEONCE, "pintool", "o", "blocktrace_main.out", "specify output file name");
+KNOB< string > KnobOutputFile(KNOB_MODE_WRITEONCE, "pintool", "o", "blocktrace_main_base.out", "specify output file name");
 KNOB< string > KnobDebugFile(KNOB_MODE_WRITEONCE, "pintool", "d", "blocktrace_main_debug.out", "specify debug file name");
 
 // This function is called when the application exits
@@ -107,6 +119,8 @@ int main(int argc, char* argv[])
 
     OutFile.open(KnobOutputFile.Value().c_str());
     DBGFile.open(KnobDebugFile.Value().c_str());
+
+    PIN_InitSymbols(); // Call this early in your tool's initialization
 
     // Register Instruction to be called to instrument instructions
     TRACE_AddInstrumentFunction(Trace, 0);
